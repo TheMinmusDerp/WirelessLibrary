5a13ba724a523860d21d2222af5a1c99a11b57b5
TheMinmusDerp <71770907+TheMinmusDerp@users.noreply.github.com>
2021-05-06 08:10:44 -0500

    Add files via upload

---
+++ screens/SearchScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native'
+
+export default class SearchScreen extends React.Component {
+    render(){
+        return(
+            <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+                <Text>S E A R C H</Text>
+            </View>
+        )
+    }
+}
